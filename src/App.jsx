16bc5c98613c5673f40851f4dd22f4d5bfa6079b
Sev7n <ntoan2884@gmail.com>
2024-06-21 18:15:45 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThreeScene from './components/ThreeScene';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Trang web tương tác 3D</h1>
+      <ThreeScene />
+    </div>
+  );
+};
+
+export default App;
